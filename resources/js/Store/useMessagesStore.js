5145b95ab3c5539a9cfa7e51ee6281fb0bc52976
Guillaume Bourdages <gbourdages@graphem.ca>
2024-09-15 11:16:38 -0700

    Adjusted to use Ably and not reverb

--- resources/js/Store/useMessagesStore.js
+++ resources/js/Store/useMessagesStore.js
@@ -1,45 +1,50 @@
 import { defineStore } from "pinia";
-import axios from 'axios'
+import axios from "axios";
 
-export const useMessagesStore = defineStore('messages', {
+export const useMessagesStore = defineStore("messages", {
     state: () => ({
         page: 1,
-        messages: []
+        messages: [],
     }),
 
     actions: {
-        fetchState (roomSlug, page = 1) {
-            axios.get(`/rooms/${roomSlug}/messages?page=${page}`)
-                .then(response => {
-                    this.page = response.data.meta.current_page
-                    this.messages = [...this.messages, ...response.data.data]
+        fetchState(roomSlug, page = 1) {
+            axios
+                .get(`/rooms/${roomSlug}/messages?page=${page}`)
+                .then((response) => {
+                    this.page = response.data.meta.current_page;
+                    this.messages = [...this.messages, ...response.data.data];
+                });
+        },
+
+        fetchPreviousState(roomSlug) {
+            this.fetchState(roomSlug, this.page + 1);
+        },
+
+        pushMessage(message) {
+            this.messages.pop();
+            this.messages = [message, ...this.messages];
+        },
+
+        storeMessage(roomSlug, payload) {
+            console.log(window.Echo.socketId());
+            axios
+                .post(`/rooms/${roomSlug}/messages`, payload, {
+                    headers: {
+                        "X-Socket-Id": Echo.socketId(),
+                    },
                 })
+                // axios
+                //     .post(`/rooms/${roomSlug}/messages`, payload)
+                .then((response) => {
+                    this.pushMessage(response.data);
+                });
         },
-
-        fetchPreviousState (roomSlug) {
-            this.fetchState(roomSlug, this.page + 1)
-        },
-
-        pushMessage (message) {
-            this.messages.pop()
-            this.messages = [message, ...this.messages]
-        },
-
-        storeMessage (roomSlug, payload) {
-            axios.post(`/rooms/${roomSlug}/messages`, payload, {
-                headers: {
-                    'X-Socket-Id': Echo.socketId()
-                }
-            })
-                .then(response => {
-                    this.pushMessage(response.data)
-                })
-        }
     },
 
     getters: {
-        allMessages (state) {
-            return state.messages
-        }
-    }
-})
+        allMessages(state) {
+            return state.messages;
+        },
+    },
+});
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,16 +1,29 @@
-import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo';
 
-import Pusher from 'pusher-js';
-import axios from "axios";
-window.Pusher = Pusher;
+// import Pusher from 'pusher-js';
+// import axios from "axios";
+// window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
+// });
+
+import Echo from '@ably/laravel-echo';
+import * as Ably from 'ably';
+
+window.Ably = Ably; // make globally accessible to Echo
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    broadcaster: 'ably',
 });
 
+window.Echo.connector.ably.connection.on(stateChange => {
+    if (stateChange.current === 'connected') {
+        console.log('connected to ably server');
+    }
+});
